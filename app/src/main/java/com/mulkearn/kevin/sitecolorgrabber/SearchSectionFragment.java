008994a5e992f46fd7fa309d2eb826899355cb5f
Kevin Mulkearn <kevinmulkearn@gmail.com>
2018-01-17 17:17:05 +0000

    Keyboard entry search button. Keyboard minimize after search button.

--- app/src/main/java/com/mulkearn/kevin/sitecolorgrabber/SearchSectionFragment.java
+++ app/src/main/java/com/mulkearn/kevin/sitecolorgrabber/SearchSectionFragment.java
@@ -1,13 +1,15 @@
 package com.mulkearn.kevin.sitecolorgrabber;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.app.Activity;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -36,20 +38,43 @@ public class SearchSectionFragment extends Fragment{
         searchBarInput = (EditText) view.findViewById(R.id.searchBarInput);
         final Button button = (Button) view.findViewById(R.id.searchButton);
 
+        //On Keyboard Enter Click
+        searchBarInput.setOnKeyListener(
+                new View.OnKeyListener() {
+                    @Override
+                    public boolean onKey(View v, int keyCode, KeyEvent event) {
+                        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                            switch (keyCode) {
+                                case KeyEvent.KEYCODE_DPAD_CENTER:
+                                case KeyEvent.KEYCODE_ENTER:
+                                buttonClicked(v);
+                                InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                                return true;
+                            default:
+                                break;
+                            }
+                        }
+                    return false;
+                    }
+                }
+        );
+
+        //On button Click
         button.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         buttonClicked(v);
+                        InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
                     }
                 }
         );
         return view;
     }
 
-
     public void buttonClicked(View view){
         activityCommander.createMeme(searchBarInput.getText().toString());
     }
-
 }
